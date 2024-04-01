16d46ede56a7417a4a056dae3371d306344f5eaa
NguyenThiHien6 <20130257@st.hcmuaf.edu.vn>
2024-04-01 22:01:14 +0700

    giao dien dki dnhap

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,11 +1,87 @@
-import React from 'react'
+import React from "react";
+import registerStyles from '../css/register.module.css'
 
-type Props = {}
 
-const Register = (props: Props) => {
+
+const Register = () => {
   return (
-    <div>Register</div>
-  )
-}
+    <div className={registerStyles.body}>
+      <div class={registerStyles.main}>
+        <h2 style={{textAlign: "center"}}>Đăng ký</h2>
+        <form action="">
+          <label for="first"
+          >Họ và tên:</label
+          >
+          <input
+            type="text"
+            id="first"
+            name="first"
+            required
+          />
 
-export default Register
+          <label for="last"
+          >Tên tài khoản:</label
+          >
+          <input
+            type="text"
+            id="last"
+            name="last"
+            required
+          />
+
+          <label for="email">Email:</label>
+          <input
+            type="email"
+            id="email"
+            name="email"
+            required
+          />
+
+          <label for="password"
+          >Mật khẩu:</label
+          >
+          <input
+            type="password"
+            id="password"
+            name="password"
+            pattern="^(?=.*\d)(?=.*[a-zA-Z])(?=.*[^a-zA-Z0-9])\S{8,}$"
+            title="Password must contain at least one number,
+						one alphabet, one symbol, and be at
+						least 8 characters long"
+            required
+          />
+
+          <label for="repassword"
+          >Nhập lại mật khẩu:</label
+          >
+          <input
+            type="password"
+            id="repassword"
+            name="repassword"
+            required
+          />
+
+          <label for="mobile"
+          >Số điện thoại:</label
+          >
+          <input
+            type="text"
+            id="mobile"
+            name="mobile"
+            maxlength="10"
+            required
+          />
+         
+
+          <button type="submit">
+            Đăng Ký
+          </button>
+          <p >Bạn đã có tài khoản <a href="">Đăng Nhập</a></p>
+        </form>
+      </div>
+    </div>
+
+  );
+};
+
+export default Register;
--- src/routes/ContainerRoutes.jsx
+++ src/routes/ContainerRoutes.jsx
@@ -8,13 +8,13 @@ import Register from "../pages/Register";
 import ForgotPassword from "../pages/ForgotPassword";
 import Home from "../pages/Home";
 
-type Props = {};
 
-const ContainerRoutes = (props: Props) => {
+
+const ContainerRoutes = () => {
   return (
     <Routes>
       <Route path="login" element={<Login />} />
-      <Route path="" element={<Home />} />
+      <Route path="home" element={<Home />} />
       <Route path="register" element={<Register />} />
       <Route path="forgot-password" element={<ForgotPassword />} />
     </Routes>
